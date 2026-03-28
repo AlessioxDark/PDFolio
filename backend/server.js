213251296c73dddd7c149647c19b83e3f5d19b30
Alessio Quaranta <alessio40aq@gmail.com>
2026-03-28 19:37:05 +0100

    aggiunta struttura cartelle ( anche se non visibile )

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
-const supabase = require("./config/db");
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Funziona!</h1>");
+});
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
 // await supabase.from("test").insert([{ col: "es" }]);
